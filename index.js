b39fb6bdee61ea90051a75620210a2e1a162f491
Hector-Luis <hluischimbo@gmail.com>
2018-08-23 10:47:59 -0500

    logito

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+var fortune = require('./lib/fortune.js');
+
+app.get('/about', function(req, res){
+    res.render('about', {fortune: fortune.getFortune()});
+});
+
+app.use(express.static(path.join(__dirname,'/public')));
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
